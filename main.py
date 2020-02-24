2721b76049cdcdd78013b020e52eb11c12f44b57
Rohitbohra <Rohitbohra23051994@gmail.com>
2020-02-24 06:39:16 +0530

    Changes for gcp

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
+from flask import Flask,request,jsonify,render_template,url_for
+
 import packages_to_import as p
-#from understanding_data import understanding_data
+from understanding_data import understanding_data
 import cleaning_and_preparing_train_data
 import training
 from training import *
-import time
 
-start = time.time()
+
+# we can add the training data and uncomment the below lines
 # data=p.pd.read_csv("Reviews.csv")
+# data = data.sample(frac =0.6)
 
 
+# to undersatnd the data and for cleaning of training data we can uncomment the below code
 #understanding_data(data)
-# cleaning_and_preparing_train_data.df(data)
-text_query=input("Enter the review : ")
-test_data = p.pd.DataFrame(p.np.column_stack([text_query]),columns=['Text'])
-tt=train_test(test_data)
-# tt.train()
+#cleaning_and_preparing_train_data.df(data)
+# summary_query=input("Enter the summary")
+# text_query=input("Enter the review")
 
-tt.testing()
-predicted=tt.testing()
-predicted="Positive" if predicted > 0.5 else "Negative"
-print("The review is",predicted)
 
-end = time.time()
-print(f"Runtime of the program is {end - start}")
+
+app=Flask(__name__)
+
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    if request.method == 'POST':
+        message=request.form['Text']
+        test_data = p.pd.DataFrame(p.np.column_stack([message]),columns=['Text'])
+        tt = train_test(test_data)
+        tt.testing()
+        my_prediction = tt.testing()
+        my_prediction = 1 if my_prediction >= 0.5 else 0
+    return render_template('result.html',prediction=my_prediction)
+
+
+if __name__ =="__main__":
+    app.run(use_reloader=True, use_debugger=True,host="0.0.0.0",threaded=False)
